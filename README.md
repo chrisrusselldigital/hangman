0e353552007fe54400983a8cc3efadcb6ab11c19
Chris Russell <chris.john.russell@gmail.com>
2020-11-09 22:01:38 +0000

    creates some words

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hangman
+
+A word game.
---
+++ hangman.js
@@ -0,0 +1,3 @@
+var words = ["spot", "panda", "flower", "boat", "trousers", "skirt", "tennis", "balcony", "table", "chair", "pot", "cupboard", "notebook", "cushion"];
+
+var randomElement = words[Math.floor(Math.random() * words.length)];
