71f0fafb6b322b13099eef0974277c91ef3f1836
Chris Russell <chris.john.russell@gmail.com>
2020-11-13 16:39:21 +0000

    adds word and form

--- hangman.js
+++ hangman.js
@@ -1,3 +1,20 @@
 var words = ["spot", "panda", "flower", "boat", "trousers", "skirt", "tennis", "balcony", "table", "chair", "pot", "cupboard", "notebook", "cushion"];
 
-var randomElement = words[Math.floor(Math.random() * words.length)];
+var randomWord = words[Math.floor(Math.random() * words.length)];
+
+
+function SplitWord() {
+  aSplitWord = randomWord.split("");
+  console.log(aSplitWord);
+}
+
+function DashesOfWord() {
+  console.log(randomWord);
+  console.log(randomWord.length);
+  for (var i = 0; i < randomWord.length; i++) {
+    document.getElementById("lines").innerHTML += " &#95 ";
+  }
+}
+
+SplitWord();
+DashesOfWord();
